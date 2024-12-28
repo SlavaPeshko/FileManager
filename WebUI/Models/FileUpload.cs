7256cb26f882c614de9e884e1af3195a3e1c4cd1
viachaslau peshka <slava.peshko@gmail.com>
2024-12-28 12:32:54 +0100

    update package-lock.json

---
+++ WebUI/Models/FileUpload.cs
@@ -0,0 +1,9 @@
+﻿namespace WebUI.Models;
+
+public record FileUpload
+{
+    public required string FileName { get; init; }
+    public required byte[] Content { get; init; }
+    public required int UserId { get; init; }
+    public required string ConnectionId { get; init; }
+}
